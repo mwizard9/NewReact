691bfd8862e21fba1657bd8d2065c88cbfcb214f
Manish Himalaya <manishhimalaya64@gmain.com>
2022-12-26 16:04:41 +0545

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-let name="Manish";
+
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="container">
-      <h1>Hello {name}</h1>
-     
+   <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Mwizard</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+       
+      </ul>
     </div>
+  </div>
+</nav>
     </>
   );
 }
