41e4b3cdb8e5c98c7b2e60ed635a5909c4db5a63
Manish Himalaya <manishhimalaya64@gmain.com>
2022-12-26 15:28:19 +0545

    nav

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 let name="Manish";
 function App() {
   return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
     <div className="container">
       <h1>Hello {name}</h1>
      
     </div>
+    </>
   );
 }
 
