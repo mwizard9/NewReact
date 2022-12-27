05b00f82a1126249be1efc09a471ce526d1ed60a
Manish Himalaya <manishhimalaya64@gmail.com>
2022-12-27 16:11:33 +0545

    addingform

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Form from './Components/Form';
 
 
 function App() {
   return (
     <>
 <Navbar title="MwizarDon" About="About Us" Home="Home"/>
+<Form/>
     </>
   );
 }
---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div>
+        <div className="mb-3">
+            <label for="mybox" className="form-label">Example textarea</label>
+            <textarea className="form-control" id="mybox" rows="3"></textarea>
+        </div>
+    </div>
+  )
+}
