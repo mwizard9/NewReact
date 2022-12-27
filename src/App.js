674bfe89ab8dc54d0e8a8c93cae64f11eb6b972b
Manish Himalaya <manishhimalaya64@gmain.com>
2022-12-27 14:06:50 +0545

    new branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
 <Navbar title="MwizarDon"/>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur sint, culpa dolore tempora ullam vel, vitae laudantium nisi enim accusantium praesentium harum facere deserunt, accusamus molestias minima unde hic reiciendis!</p>
     </>
   );
 }
