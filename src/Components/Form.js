f007e0893334af7c57161cc1db0b8fde6c3ee67e
Manish Himalaya <manishhimalaya64@gmail.com>
2022-12-28 17:08:16 +0545

    lowercase

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,6 +6,11 @@ export default function Form(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLwClick = ()=>{
+      // console.log("button was clicked" + text)
+       let newText = text.toLowerCase();
+       setText(newText)
+   }
     const handleOnChange = (event)=>{
        // console.log("On change")
         setText(event.target.value)
@@ -21,12 +26,15 @@ export default function Form(props) {
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
         </div>
-        <buttom className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</buttom>
+        <buttom className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</buttom>
+        <buttom className="btn btn-primary mx-1" onClick={handleLwClick}>Convert to Lowercase</buttom>
     </div>
     <div className="container my-3">
       <h1>Your text summary</h1>
       <p>{text.split(" ").length} words, {text.length} characters</p>
       <p>{0.008* text.split(" ").length} minutes to read</p>
+      <h2>preview</h2>
+      <p>{text}</p>
     </div>
     </>
 
