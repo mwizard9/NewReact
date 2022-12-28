22788bf39838c4dc67642c3dd861c5ab0115425d
Manish Himalaya <manishhimalaya64@gmail.com>
2022-12-28 14:17:00 +0545

    adding some

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,12 +2,12 @@ import React, {useState} from 'react'
 
 export default function Form(props) {
     const handleUpClick = ()=>{
-        console.log("button was clicked" + text)
+       // console.log("button was clicked" + text)
         let newText = text.toUpperCase();
         setText(newText)
     }
     const handleOnChange = (event)=>{
-        console.log("On change")
+       // console.log("On change")
         setText(event.target.value)
     }
     
@@ -15,12 +15,19 @@ export default function Form(props) {
     const[text, setText] = useState('Enter text here');
     
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
         </div>
         <buttom className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</buttom>
     </div>
+    <div className="container my-3">
+      <h1>Your text summary</h1>
+      <p>622 words, 1800 characters</p>
+    </div>
+    </>
+
   )
 }
