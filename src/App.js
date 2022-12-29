c6df06e1576c11e748c4151fa616eefb9b15edd1
Manish Himalaya <manishhimalaya64@gmail.com>
2022-12-29 14:54:21 +0545

    adding swith

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import { useState } from 'react';
 
 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode,setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode("dark");
+      document.body.style.backgroundColor = "gray"
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white"
+    }
+  }
   return (
     <>
-<Navbar title="MwizarDon" About="About Us" Home="Home" mode={mode}/>
+<Navbar title="MwizarDon" About="About Us" Home="Home" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
 <Form heading="Enter the text to Analyze"/>
 {/* <About/> */}
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -26,6 +26,11 @@ export default function Navbar(props) {
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <div className={`form-check form-switch mx-3 text-${props.mode==='light'?'dark':'light'}`}>
+  <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Darkmode</label>
+</div>
+
   
   </nav>
   )
