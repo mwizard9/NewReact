086281d03531fb09fce4f426dd4f4595270101bb
Manish Himalaya <manishhimalaya64@gmail.com>
2022-12-29 14:29:47 +0545

    setting navbar's background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Form from './Components/Form';
+import { useState } from 'react';
 //import About from './Components/About';
 
 
 function App() {
+  const [mode, setMode] = useState('light');
   return (
     <>
-<Navbar title="MwizarDon" About="About Us" Home="Home"/>
+<Navbar title="MwizarDon" About="About Us" Home="Home" mode={mode}/>
 <div className="container my-3">
 <Form heading="Enter the text to Analyze"/>
 {/* <About/> */}
